53c92a5ca2b10d5ddb7ddb3190d215ad570d5749
Adam James McCullough <merlin@fb.com>
2018-06-04 11:01:39 -0600

    Refactors + typo fixes.

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Monad (join)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Identity
 import Control.Monad.Trans.Maybe
 
@@ -18,34 +19,40 @@ itBindTryOne (IdentityT fa) ab = fmap (ab) fa
 iTBind :: Monad f => IdentityT f a -> (a -> IdentityT f b) -> IdentityT f b
 iTBind (IdentityT fa) ab = IdentityT $ join $ fmap (runIdentityT . ab) fa
 
+myPrompt :: String -> IO String
+myPrompt prompt = do
+  putStr prompt
+  result <- getLine
+  return result
+
 getName :: MaybeT IO String
 getName = do
-  input <- liftIO getLine
+  input <- lift $ myPrompt "Name? "
   if input == ""
     then MaybeT $ return Nothing
     else MaybeT $ return $ Just $ "Name: " ++  input ++ "\n"
   
 getNumber :: String -> MaybeT IO String
 getNumber str = do
-  input <- liftIO getLine
+  input <- lift $ myPrompt "Phone number? "
   if (length input) /= 7
     then MaybeT $ return Nothing
     else MaybeT $ return $ Just $ str ++ "Phone Number: " ++ input ++ "\n" 
 
 getStreetName :: String -> MaybeT IO String
 getStreetName str = do
-  input <- liftIO getLine
+  input <- lift $ myPrompt "Street Name? "
   if input == ""
     then MaybeT $ return Nothing
     else MaybeT $ return $ Just $ str ++ "Street Name: " ++ input ++ "\n"
 
-compositionMethod :: IO (Maybe String)
-compositionMethod = runMaybeT $ getName >>=
-                               getNumber >>=
-                               getStreetName
+compositionMethod :: MaybeT IO (String)
+compositionMethod = getName >>=
+                    getNumber >>=
+                    getStreetName
 
-compAlt :: IO (Maybe String)
-compAlt = runMaybeT $ do
+compAlt :: MaybeT IO (String)
+compAlt = do
   a <- getName
   b <- getNumber a
   c <- getStreetName b
@@ -53,14 +60,17 @@ compAlt = runMaybeT $ do
 
 allTogetherNow :: IO ()
 allTogetherNow = do
-  result <- compositionMethod
+  result <- runMaybeT compositionMethod
   case result of
     Just str -> putStrLn str
     Nothing -> putStrLn "Failure!"
 
 aTNv2 :: IO ()
 aTNv2 = do
-  result <- compAlt
+  result <- runMaybeT compAlt
   case result of
     Just str -> putStrLn str
     Nothing -> putStrLn "Failure!"
+
+
+
